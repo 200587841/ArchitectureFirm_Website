ebb6890ea3a231ea520bb2417831f64ae4f6805c
200587841 <157327015+200587841@users.noreply.github.com>
2024-07-19 17:29:31 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <meta name="author"
+        content="André Argiles Azoubel / Jhon Sebastian Reina Gomez / Lucas de Magalhaes e Castro / Natalia Selestrini Dias" />
+    <meta name="description" content="Main Project: Selling Out!">
+    <link rel="shortcut icon" href="./images/logo.png" type="image/x-icon" />
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+
+<body>
+    <div id="navbar">
+        <?php
+        include 'includes/global-nav.php';
+        ?>
+    </div>
+</body>
+
+<footer>
+    <?php
+    include 'includes/footer-nav.php';
+    ?>
+</footer>
+
+</html>
