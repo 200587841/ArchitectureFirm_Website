387a80e4a31fbfa98eb2596ae3b504b65204aefa
LucasNavim <118214949+LucasNavim@users.noreply.github.com>
2024-07-25 08:53:00 -0400

    Add files via upload

---
+++ includes/footer-navb.php
@@ -0,0 +1,12 @@
+<footer class="black-footer">
+    <div class="footer-content">
+        <p>123, Anywhere Dr, Canada | (123) 456-7890 | contact@alns.ca</p>
+        <div class="social-icons">
+            <a href="#"><img src="fb-b.svg" alt="Facebook"></a>
+            <a href="#"><img src="inst-b" alt="Instagram"></a>
+            <a href="#"><img src="pin-b" alt="Pinterest"></a>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
---
+++ includes/footer-navg.php
@@ -0,0 +1,12 @@
+<footer class="green-footer">
+    <div class="footer-content">
+        <p>123, Anywhere Dr, Canada | (123) 456-7890 | contact@alns.ca</p>
+        <div class="social-icons">
+            <a href="#"><img src="fb-g.svg" alt="Facebook"></a>
+            <a href="#"><img src="inst-g" alt="Instagram"></a>
+            <a href="#"><img src="pin-g" alt="Pinterest"></a>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
