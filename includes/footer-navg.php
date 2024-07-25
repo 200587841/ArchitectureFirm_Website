e31a659433ff7c2b16b6d4cbf723fc2c48fc5b90
LucasNavim <118214949+LucasNavim@users.noreply.github.com>
2024-07-25 09:50:46 -0400

    footer-navg.php

--- includes/footer-navg.php
+++ includes/footer-navg.php
@@ -1,10 +1,10 @@
 <footer class="green-footer">
     <div class="footer-content">
-        <p>123, Anywhere Dr, Canada | (123) 456-7890 | contact@alns.ca</p>
+        123, Anywhere Dr, Canada | (123) 456-7890 | contact@alns.ca
         <div class="social-icons">
-            <a href="#"><img src="fb-g.svg" alt="Facebook"></a>
-            <a href="#"><img src="inst-g" alt="Instagram"></a>
-            <a href="#"><img src="pin-g" alt="Pinterest"></a>
+            <a href="#"><img src="images/fb-g.svg" alt="Facebook"></a>
+            <a href="#"><img src="images/inst-g.svg" alt="Instagram"></a>
+            <a href="#"><img src="images/pin-g.svg" alt="Pinterest"></a>
         </div>
     </div>
 </footer>
