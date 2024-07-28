264145bf1b246a0fddd3d8dbb84271600b712b30
AAAzoubel <122544918+AAAzoubel@users.noreply.github.com>
2024-07-27 22:38:54 -0400

    Add files via upload

---
+++ includes/global-nav.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="home.php">HOME</a></li>
+                <li><a href="about.php">ABOUT</a></li>
+                <li><a href="projects.php">PROJECTS</a></li>
+                <li><a href="contact.php">CONTACT</a></li>
+            </ul>
+        </nav>
+    </header>
