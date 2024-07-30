55dc6721a82b61c9bc29f44113c4194b76eed786
sebasreina1 <87530099+sebasreina1@users.noreply.github.com>
2024-07-30 18:39:21 -0400

    projects.php

--- projects.php
+++ projects.php
@@ -2,11 +2,10 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projects</title>
-    <meta name="author"
-        content="André Argiles Azoubel / Jhon Sebastian Reina Gomez / Lucas de Magalhaes e Castro / Natalia Selestrini Dias" />
+    <title>Project Showcase</title>
+    <meta name="author" content="André Argiles Azoubel / Jhon Sebastian Reina Gomez / Lucas de Magalhaes e Castro / Natalia Selestrini Dias" />
     <meta name="description" content="Main Project: Selling Out!">
     <link rel="shortcut icon" href="./images/logo.png" type="image/x-icon" />
     <link rel="stylesheet" href="./css/styles.css">
@@ -14,16 +13,53 @@
 
 <body>
     <div id="navbar">
-        <?php
-        include 'includes/global-nav.php';
-        ?>
+        <?php include 'includes/global-nav.php'; ?>
     </div>
-</body>
+    
+    <main>
+        <section class="projects">
+            <div class="project">
+                <img src="img/project1.png" alt="Project 1">
+                <div class="overlay">
+                    <p>Casa da Mangueira</p>
+                </div>
+            </div>
+            <div class="project">
+                <img src="img/project2.png" alt="Project 2">
+                <div class="overlay">
+                    <p>Hammok House</p>
+                </div>
+            </div>
+            <div class="project">
+                <img src="img/project3.png" alt="Project 3">
+                <div class="overlay">
+                    <p>LEnS House</p>
+                </div>
+            </div>
+            <div class="project">
+                <img src="img/project4.png" alt="Project 4">
+                <div class="overlay">
+                    <p>Casa do Parque House</p>
+                </div>
+            </div>
+            <div class="project">
+                <img src="img/project5.png" alt="Project 5">
+                <div class="overlay">
+                    <p>3V House</p>
+                </div>
+            </div>
+            <div class="project">
+                <img src="img/project6.png" alt="Project 6">
+                <div class="overlay">
+                    <p>Mipibu House</p>
+                </div>
+            </div>
+        </section>
+    </main>
 
-<footer>
-    <?php
-    include 'includes/footer-navg.php';
-    ?>
-</footer>
+    <footer>
+        <?php include 'includes/footer-navg.php'; ?>
+    </footer>
+</body>
 
 </html>
