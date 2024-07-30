d6b2cc56c5c845661cb7e46e6812872655587845
LucasNavim <118214949+LucasNavim@users.noreply.github.com>
2024-07-30 18:47:26 -0400

    about.php

--- about.php
+++ about.php
@@ -18,6 +18,39 @@
         include 'includes/global-nav.php';
         ?>
     </div>
+    <section class="about-us-section">
+        <div class="top-box">
+            <div class="column">
+                <div class="title-about">
+                <h1>ABOUT US</h1>
+                </div>
+            </div>
+            <div class="column">
+                <h1>&nbsp;</h1> <!-- Empty h1 to align text vertically -->
+                <p>
+                    Our signature color green symbolizes our commitment to sustainable development. At Alns, great architecture is sustainable architecture. Join us in building a greener world, one project at a time.
+                    Welcome to Alns, where innovation meets sustainability. We create architectural marvels that harmonize with the environment, with sustainability at the heart of everything we do.             
+                </p>
+            </div>
+            <div class="column">
+                <h1>&nbsp;</h1> <!-- Empty h1 to align text vertically -->
+                <p>
+                Alns combines cutting-edge technology with eco-friendly practices, minimizing ecological footprints and maximizing energy efficiency. Our projects span residential, commercial, and industrial sectors, providing innovative solutions that meet the highest standards of quality and sustainability. Join us on a journey to a better, greener future.
+                </p>
+            </div>
+        </div>
+        <div class="bottom-box">
+            <div class="column">
+                <img src="./images/about1.jpg" alt="Architects working" class="about-us-image">
+            </div>
+            <div class="column">
+                <img src="./images/about2.jpg" alt="Team planning" class="about-us-image">
+            </div>
+            <div class="column">
+                <img src="./images/about3.jpg" alt="Design process" class="about-us-image">
+            </div>
+        </div>
+    </section>
 </body>
 
 <footer>
