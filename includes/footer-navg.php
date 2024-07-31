1a0b02d8c291fdce8b227aff9da38642c5ce5376
200587841 <157327015+200587841@users.noreply.github.com>
2024-07-31 09:13:24 -0400

    Add files via upload

--- includes/footer-navg.php
+++ includes/footer-navg.php
@@ -1,10 +1,10 @@
 <footer class="green-footer">
     <div class="footer-content">
-        123, Anywhere Dr, Canada | (123) 456-7890 | contact@alns.ca
-        <div class="social-icons">
-            <a href="#"><img src="images/fb-g.svg" alt="Facebook"></a>
-            <a href="#"><img src="images/inst-g.svg" alt="Instagram"></a>
-            <a href="#"><img src="images/pin-g.svg" alt="Pinterest"></a>
-        </div>
+        <p>&copy; 123, Anywhere Dr, Canada | (123) 456-7890 | contact@alns.ca </p>
+            <div class="social-icons">
+                <a href="#"><img src="images/fb-g.svg" alt="Facebook"></a>
+                <a href="#"><img src="images/inst-gr.svg" alt="Instagram"></a>
+                <a href="#"><img src="images/pin-g.svg" alt="Pinterest"></a>
+            </div>
     </div>
 </footer>
--- includes/global-nav.php
+++ includes/global-nav.php
@@ -1,12 +1,13 @@
-Ï   <header id="navbar">
-        <div class="logo">
-        <a href="#"><img src="images/logo.png" alt="Logo"></a>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="home.php">HOME</a></li>
-                <li><a href="about.php">ABOUT</a></li>
-                <li><a href="projects.php">PROJECTS</a></li>
-                <li><a href="contact.php">CONTACT</a></li>
-            </ul>
-        </nav>
+<div id="navbar">
+    <div class="logo">
+        <a href="home.php"><img src="images/logo.png" alt="Logo"></a>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="home.php">HOME</a></li>
+            <li><a href="about.php">ABOUT</a></li>
+            <li><a href="projects.php">PROJECTS</a></li>
+            <li><a href="contact.php">CONTACT</a></li>
+        </ul>
+    </nav>
+</div>
